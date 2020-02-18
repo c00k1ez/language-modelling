9eae64ba9529b0c6830d883a021482aac09f1d98
c00k1ez <egorplotnikov@mail.ru>
2020-02-18 19:12:29 +0300

    fix no.4

--- framework/models.py
+++ framework/models.py
@@ -27,19 +27,17 @@ class ClassicLanguageModel(nn.Module):
     def __init__(self, vocab_size, embeddig_dim, hidden_size):
         super(ClassicLanguageModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embeddig_dim)
-        self.emb_do = nn.Dropout(0.3)
         self.lstm = nn.LSTM(embeddig_dim, hidden_size, batch_first=True)
-        self.do1 = SpatialDropout()
+        self.do = SpatialDropout()
         self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
-        self.do2 = SpatialDropout()
         self.norm = nn.LayerNorm(hidden_size)
         self.head = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, batch):
         emb = self.embedding(batch)
-        emb = self.emb_do(emb)
-        lstm_output = self.do1(self.lstm(emb)[0])
-        gru_output = self.do2(self.gru(lstm_output)[0])
+        emb = self.do(emb)
+        lstm_output = self.do(self.lstm(emb)[0])
+        gru_output = self.do(self.gru(lstm_output)[0])
         pre_head = lstm_output + gru_output
         pre_head = self.norm(pre_head)
         return F.log_softmax(self.head(pre_head), dim=2)
